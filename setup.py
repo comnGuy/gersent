ab68b5a4fd9ddc292d1a4c2721cdf6e91485d6d9
Bernhard Preisler <you@example.com>
2019-11-16 14:35:07 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gersent",  # Replace with your own username
-    version="0.0.3",
+    version="0.0.5",
     author="Bernhrd Preisler",
     author_email="bpblub@gmail.com",
     description="Easy german sentiment",
