40e50c68d62d746adcdc0f2b268e5d5ba158f580
Bernhard Preisler <bpreisler@nero.com>
2019-11-28 20:15:25 +0100

    Comments

--- gersent/gersent.py
+++ gersent/gersent.py
@@ -25,34 +25,63 @@ class SimpleSentiment:
     WORDLIST_POSITIVE_PATH = "wordlists/de/positive.json"
     WORDLIST_NEGATIVE_PATH = "wordlists/de/negative.json"
 
-    def __init__(self):
-        _this_module_file_path_ = os.path.abspath(getsourcefile(lambda: 0))
-        self.WORDLIST_POSITIVE_PATH = os.path.join(
-            os.path.dirname(_this_module_file_path_), 
-            self.WORDLIST_POSITIVE_PATH
-        )
-        self.WORDLIST_NEGATIVE_PATH = os.path.join(
-            os.path.dirname(_this_module_file_path_), 
-            self.WORDLIST_NEGATIVE_PATH
-        )
+    PREPROCESSING_REPLACEMENTS = [".", ",", "!", '"', "$", "_"]
 
+    def __init__(self):
         self.wordlist_negative = {}
         self.wordlist_positiv = {}
 
+        self._generate_pathes()
         self._load()
 
+    def _generate_pathes(self):
+        """ Generats pathes for the wordlists """
+        _this_module_file_path_ = os.path.abspath(getsourcefile(lambda: 0))
+        self.WORDLIST_POSITIVE_PATH = os.path.join(
+            os.path.dirname(_this_module_file_path_), self.WORDLIST_POSITIVE_PATH
+        )
+        self.WORDLIST_NEGATIVE_PATH = os.path.join(
+            os.path.dirname(_this_module_file_path_), self.WORDLIST_NEGATIVE_PATH
+        )
+
     def _load(self):
+        """ Loads wordlists """
         self.wordlist_negative = self._load_json(self.WORDLIST_NEGATIVE_PATH)
         self.wordlist_positiv = self._load_json(self.WORDLIST_POSITIVE_PATH)
 
     def _load_json(self, path: str):
+        """ Loads a json file 
+        
+        Args:
+            path: Path to the file
+        
+        Return:
+            wordlist: A json wordlist | dict
+        """
+
         with open(path, "r") as fp:
             return json.load(fp)
 
     def sentiment(self, sentence: str):
+        """ Calls the algorithm
+        
+        Args:
+            sentence: The sentence which will be calculated
+
+        Return:
+            sentiment: The results of the sentence | float
+        """
         return self._algorithm(sentence)
 
     def _algorithm(self, sentence: str):
+        """ Performs the algorithm
+        
+        Args:
+            sentence: The sentence which will be calculated
+
+        Return:
+            sentiment: The results of the sentence | float
+        """
         sentence_preprocessed = self._preprocessing(sentence)
         negative_score = self._evaluate(sentence_preprocessed, self.wordlist_negative)
         positive_score = self._evaluate(sentence_preprocessed, self.wordlist_positiv)
@@ -70,7 +99,16 @@ class SimpleSentiment:
             "composite": (negative_score + positive_score) * negation,
         }
 
-    def _evaluate(self, sentence, wordlist):
+    def _evaluate(self, sentence: str, wordlist: dict):
+        """ Performs the wordlist 
+        
+        Args:
+            sentence: The sentence which will be calculated
+            wordlist: Dictionary of a wordlist
+
+        Return:
+            sentiment: The results of the sentence | float
+        """
         words = sentence.split(" ")
         score = 0.0
         for word in words:
@@ -79,18 +117,52 @@ class SimpleSentiment:
         return score
 
     def _preprocessing(self, sentence: str):
-        return sentence.replace(".", "").replace("!", "").replace(",", "")
+        """ Preprocess the sentence 
+        
+        Args:
+            sentence: The sentence which will be calculated
+        
+        Return:
+            sentence_preprocessed: The preprocessed sentence | str
+        """
+        for char_ in self.PREPROCESSING_REPLACEMENTS:
+            sentence = sentence.replace(char_, "")
+        return sentence
 
     def multiplier(self, sentence: str):
+        """ Computes a bunch of multiplier
+        
+        Args:
+            sentence: The sentence which will be calculated
+        
+        Return:
+            multiplier: Calculated multiplier of the sentence | float
+        """
         multiplier = 1.0
         multiplier += self.last_symbol(sentence)
         return multiplier
 
     def last_symbol(self, sentence):
+        """ Count "!" for the multiplier
+        
+        Args:
+            sentence: The sentence which will be calculated
+        
+        Return:
+            multiplier for "!"
+        """
         return sentence.count("!") * 0.05
 
     # TODO (Bernhard): Maybe relate nicht to the next value
     def _negation(self, sentence):
+        """ Inverse the meaning of the sentence
+        
+        Args:
+            sentence: The sentence which will be calculated
+        
+        Return:
+            inverse: Negation of the meaning | integer (0|1)
+        """
         words = sentence.split(" ")
         for word in words:
             if word == "nicht":
